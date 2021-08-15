548af2c84836c0692e2671635a82ef27f81f764f
AndresTorrico09 <andreslionel09@outlook.com>
2021-08-14 23:54:43 -0300

    add attrs to custom view

--- app/src/main/java/com/example/rickandmorty/utils/customviews/CircleView.kt
+++ app/src/main/java/com/example/rickandmorty/utils/customviews/CircleView.kt
@@ -1,6 +1,7 @@
 package com.example.rickandmorty.utils.customviews
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
@@ -13,14 +14,33 @@ class CircleView(context: Context, attributeSet: AttributeSet): View(context, at
     var centerOfX = 340F //center of circle on X axis
     var centerOfY = 340F //center of circle on Y axis
     var radiusOfCircleView = 140F
+    var isCenter = false
 
     init{
-        paint.color = ContextCompat.getColor(context, R.color.colorPrimary)
+        val attributeArray: TypedArray? = context.theme.obtainStyledAttributes(
+                attributeSet,
+                R.styleable.circleview, 0, 0
+        )
+
+        paint.color = attributeArray?.getColor(
+                R.styleable.circleview_circle_background,
+                ContextCompat.getColor(context, R.color.colorPrimary)
+        ) ?: android.R.color.black
+
+        isCenter = attributeArray?.getBoolean(
+                R.styleable.circleview_circle_background,
+                false
+        ) ?: false
+
         paint.strokeWidth = 40f
         paint.style = Paint.Style.STROKE //contructor call
     }
 
     override fun onDraw(canvas: Canvas?) {
+        isCenter?.let {
+            centerOfX = (width / 2).toFloat()
+            centerOfY = (height / 2).toFloat()
+        }
         canvas?.drawCircle(centerOfX, centerOfY, radiusOfCircleView, paint)
         super.onDraw(canvas)
 
